442e10de1250c02d8e54d5ae05146c65cc9c6291
Sergio García <57625297+sergiogarciiam@users.noreply.github.com>
2023-03-26 16:47:14 +0200

    Add erase function

--- src/colorPicker.js
+++
@@ -1,7 +0,0 @@
-let color_picker = document.querySelector(".color-picker");
-let color_picker_wrapper = document.querySelector(".wrapper-color-picker");
-
-color_picker.onchange = function () {
-  color_picker_wrapper.style.backgroundColor = color_picker.value;
-};
-color_picker_wrapper.style.backgroundColor = color_picker.value;
--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 // VARIABLES
 let size = 30;
-let mouseDown = false;
+let mouseDownBool = false;
+let eraseModeBool = false;
 
 // MENU CONTROL ERROR
 let menu = document.querySelector(".menu");
@@ -24,23 +25,58 @@ for (let i = 0; i < screen.height / size; i++) {
   }
 }
 
+// COLOR PICKER
+let color_picker = document.querySelector(".color-picker");
+let color_picker_wrapper = document.querySelector(".wrapper-color-picker");
+
+color_picker.onchange = function () {
+  color_picker_wrapper.style.backgroundColor = color_picker.value;
+  eraseModeBool = false;
+};
+color_picker_wrapper.style.backgroundColor = color_picker.value;
+
+// ERASER
+let eraser = document.querySelector(".fa-eraser");
+eraser.addEventListener("click", eraseMode);
+
 // FUNCTIONS
 function clickDown() {
-  mouseDown = true;
+  mouseDownBool = true;
 }
 
 function addColorMouseDown(e) {
-  if (mouseDown) {
+  if (mouseDownBool) {
+    if (eraseModeBool) {
+      e.target.style.backgroundColor = "#fff";
+    } else {
+      let color_picker = document.querySelector(".color-picker");
+      e.target.style.backgroundColor = color_picker.value;
+    }
+  } else {
+  }
+}
+
+function addColor(e) {
+  if (eraseModeBool) {
+    e.target.style.backgroundColor = "#fff";
+  } else {
     let color_picker = document.querySelector(".color-picker");
     e.target.style.backgroundColor = color_picker.value;
   }
 }
 
-function addColor(e) {
-  let color_picker = document.querySelector(".color-picker");
-  e.target.style.backgroundColor = color_picker.value;
+function clickUp() {
+  mouseDownBool = false;
 }
 
-function clickUp() {
-  mouseDown = false;
+function eraseMode() {
+  let eraser = document.querySelector(".fa-eraser");
+
+  if (eraseModeBool) {
+    eraser.style.color = "black";
+    eraseModeBool = false;
+  } else {
+    eraser.style.color = "blue";
+    eraseModeBool = true;
+  }
 }
