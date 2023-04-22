f574bf1653e7ae8ad0558e93c1c8a77e621b3743
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2023-04-22 09:57:38 +0200

    Add reset menu

--- src/script.js
+++ src/script.js
@@ -5,64 +5,29 @@ let eraseModeBool = false;
 let rainbowModeBool = false;
 
 // -- MAIN -- //
-let menu = document.querySelector(".menu");
-menu.addEventListener("mouseup", clickUp); // menu control error
-pencilMode();
-newCanvas(30);
+createNewCanvas(30);
+activatePencilMode();
+setupSideMenu();
+setupNewCanvasMenu();
+setupWarningMenu();
 
-// -- SIDE MENU -- //
-let color_picker = document.querySelector(".color-picker");
-let color_picker_wrapper = document.querySelector(".wrapper-color-picker");
-color_picker.onchange = pickColor;
-color_picker_wrapper.style.backgroundColor = color_picker.value;
+// -- MAIN FUNCTIONS -- //
+function activatePencilMode() {
+  let pencil = document.querySelector(".fa-pencil");
+  let rainbow = document.querySelector(".fa-rainbow");
+  let eraser = document.querySelector(".fa-eraser");
 
-let pencil = document.querySelector(".fa-pencil");
-pencil.addEventListener("click", pencilMode);
+  pencil.style.color = "#0E8C63";
+  pencilModeBool = true;
 
-let eraser = document.querySelector(".fa-eraser");
-eraser.addEventListener("click", eraseMode);
+  rainbow.style.color = "#333";
+  rainbowModeBool = false;
 
-let rainbow = document.querySelector(".fa-rainbow");
-rainbow.addEventListener("click", rainbowMode);
-
-let reset = document.querySelector(".fa-arrow-rotate-left");
-reset.addEventListener("click", resetCanvas);
-
-let plus = document.querySelector(".fa-plus");
-plus.addEventListener("click", displayNewCanvasMenu);
-
-// -- NEW CANVAS MENU -- //
-let value = document.querySelector(".value-size");
-let input = document.querySelector(".new-size-input");
-value.textContent = input.value;
-input.addEventListener("input", (event) => {
-  value.textContent = event.target.value;
-});
-
-let xmark = document.querySelector(".fa-xmark");
-xmark.addEventListener("click", closeNewCanvasMenu);
-
-let newCanvasButton = document.querySelector(".new-canvas-button");
-newCanvasButton.addEventListener("click", () => {
-  let sizeMenu = document.querySelector(".new-canvas-menu");
-  let input = document.querySelector(".new-size-input");
-  deletePixels();
-  newCanvas(parseInt(input.value));
-  sizeMenu.style.display = "none";
-
-  let canvas = document.querySelector(".canvas");
-  canvas.style.pointerEvents = "all";
-});
-
-// -- CANVAS FUNCTIONS -- //
-function deletePixels() {
-  let pixels = document.querySelectorAll(".pixel");
-  pixels.forEach((pixel) => {
-    pixel.remove();
-  });
+  eraser.style.color = "#333";
+  eraseModeBool = false;
 }
 
-function newCanvas(size) {
+function createNewCanvas(size) {
   for (let i = 0; i < screen.height / size; i++) {
     for (let j = 0; j < screen.width / size; j++) {
       let canvas = document.querySelector(".canvas");
@@ -70,6 +35,69 @@ function newCanvas(size) {
       canvas.appendChild(pixel);
     }
   }
+}
+
+function setupSideMenu() {
+  let color_picker = document.querySelector(".color-picker");
+  let color_picker_wrapper = document.querySelector(".wrapper-color-picker");
+  color_picker.onchange = pickColor;
+  color_picker_wrapper.style.backgroundColor = color_picker.value;
+
+  let menu = document.querySelector(".menu");
+  menu.addEventListener("mouseup", clickUp); // menu control error
+
+  let pencil = document.querySelector(".fa-pencil");
+  pencil.addEventListener("click", activatePencilMode);
+
+  let eraser = document.querySelector(".fa-eraser");
+  eraser.addEventListener("click", activateEraseMode);
+
+  let rainbow = document.querySelector(".fa-rainbow");
+  rainbow.addEventListener("click", activateRainbowMode);
+
+  let reset = document.querySelector(".fa-arrow-rotate-left");
+  reset.addEventListener("click", resetCanvasConfirmation);
+
+  let plus = document.querySelector(".fa-plus");
+  plus.addEventListener("click", displayNewCanvasMenu);
+}
+
+function setupNewCanvasMenu() {
+  let sizeValue = document.querySelector(".value-size");
+  let sizeInput = document.querySelector(".new-size-input");
+
+  sizeValue.textContent = sizeInput.value;
+  sizeInput.addEventListener("input", (event) => {
+    sizeValue.textContent = event.target.value;
+  });
+
+  let xmark = document.querySelector(".fa-xmark");
+  xmark.addEventListener("click", closeNewCanvasMenu);
+
+  let newCanvasButton = document.querySelector(".new-canvas-button");
+  newCanvasButton.addEventListener("click", createNewCanvasFromButton);
+}
+
+function setupWarningMenu() {
+  buttonResetYes = document.querySelector(".button-reset-yes");
+  buttonResetYes.addEventListener("click", resetCanvas);
+
+  buttonResetNo = document.querySelector(".button-reset-no");
+  buttonResetNo.addEventListener("click", closeResetCanvasConfirmation);
+}
+
+// -- OTHER FUNCTIONS -- //
+function createNewCanvasFromButton() {
+  let newCanvasMenu = document.querySelector(".new-canvas-menu");
+  let sizeInput = document.querySelector(".new-size-input");
+
+  deletePixels();
+  createNewCanvas(parseInt(sizeInput.value));
+
+  newCanvasMenu.style.display = "none";
+
+  let canvas = document.querySelector(".canvas");
+  canvas.style.pointerEvents = "all";
 }
 
 function createPixel(size) {
@@ -86,7 +114,13 @@ function createPixel(size) {
   return pixel;
 }
 
-// -- COLOR FUNCTIONS -- //
+function deletePixels() {
+  let pixels = document.querySelectorAll(".pixel");
+  pixels.forEach((pixel) => {
+    pixel.remove();
+  });
+}
+
 function clickDown() {
   mouseDownBool = true;
 }
@@ -96,13 +130,12 @@ function addColorMouseDown(e) {
     if (eraseModeBool) {
       e.target.style.removeProperty("background-color");
     } else if (rainbowModeBool) {
-      e.target.style.backgroundColor = randomColor();
+      e.target.style.backgroundColor = createRandomColor();
     } else {
       // pencilModeBool
       let color_picker = document.querySelector(".color-picker");
       e.target.style.backgroundColor = color_picker.value;
     }
-  } else {
   }
 }
 
@@ -110,7 +143,7 @@ function addColor(e) {
   if (eraseModeBool) {
     e.target.style.removeProperty("background-color");
   } else if (rainbowModeBool) {
-    e.target.style.backgroundColor = randomColor();
+    e.target.style.backgroundColor = createRandomColor();
   } else {
     // pencilModeBool
     let color_picker = document.querySelector(".color-picker");
@@ -122,32 +155,16 @@ function clickUp() {
   mouseDownBool = false;
 }
 
-function randomColor() {
+function createRandomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-// -- MENU FUNCTIONS -- //
 function pickColor() {
   let color_picker_wrapper = document.querySelector(".wrapper-color-picker");
   color_picker_wrapper.style.backgroundColor = color_picker.value;
 }
 
-function pencilMode() {
-  let pencil = document.querySelector(".fa-pencil");
-  let rainbow = document.querySelector(".fa-rainbow");
-  let eraser = document.querySelector(".fa-eraser");
-
-  pencil.style.color = "#0E8C63";
-  pencilModeBool = true;
-
-  rainbow.style.color = "#333";
-  rainbowModeBool = false;
-
-  eraser.style.color = "#333";
-  eraseModeBool = false;
-}
-
-function eraseMode() {
+function activateEraseMode() {
   let pencil = document.querySelector(".fa-pencil");
   let rainbow = document.querySelector(".fa-rainbow");
   let eraser = document.querySelector(".fa-eraser");
@@ -162,7 +179,7 @@ function eraseMode() {
   eraseModeBool = true;
 }
 
-function rainbowMode() {
+function activateRainbowMode() {
   let pencil = document.querySelector(".fa-pencil");
   let rainbow = document.querySelector(".fa-rainbow");
   let eraser = document.querySelector(".fa-eraser");
@@ -177,31 +194,39 @@ function rainbowMode() {
   eraseModeBool = false;
 }
 
+function resetCanvasConfirmation() {
+  let warningMenu = document.querySelector(".warning-menu");
+  warningMenu.style.display = "flex";
+}
+
+function closeResetCanvasConfirmation() {
+  let warningMenu = document.querySelector(".warning-menu");
+  warningMenu.style.display = "none";
+}
+
 function resetCanvas() {
   let pixels = document.querySelectorAll(".pixel");
   pixels.forEach((pixel) => {
     pixel.style.removeProperty("background-color");
   });
-}
 
-function newCanvasMenu() {
-  if (sizeMenu.style.display == "none") {
-    displayNewCanvasMenu();
-  } else {
-    closeNewCanvasMenu();
-  }
+  closeResetCanvasConfirmation();
 }
 
 function displayNewCanvasMenu() {
   let sizeMenu = document.querySelector(".new-canvas-menu");
   sizeMenu.style.display = "flex";
+
   let canvas = document.querySelector(".canvas");
   canvas.style.pointerEvents = "none";
+
+  closeResetCanvasConfirmation();
 }
 
 function closeNewCanvasMenu() {
   let sizeMenu = document.querySelector(".new-canvas-menu");
   sizeMenu.style.display = "none";
+
   let canvas = document.querySelector(".canvas");
   canvas.style.pointerEvents = "all";
 }
